3971073dca89dfe7c50cd4752a222135e4de0df8
Mayur Ugale <mayuru@amdocs.com>
2022-12-07 10:28:34 +0530

    made the call to inventory service a reactive rest api call

--- order-service/src/main/java/com/microservice/communication/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/communication/orderservice/service/OrderService.java
@@ -2,7 +2,10 @@ package com.microservice.communication.orderservice.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -15,15 +18,25 @@ public class OrderService {
     }
 
     public void postOrder() {
-        Map<String,String> result = webClient.get()
+
+
+ /*     // Blocking call
+        Map<String,String> result = new HashMap<>();
+        result = webClient.get()
                 .uri("http://localhost:8082/api/inventory/all")
                 .retrieve()
                 .bodyToMono(Map.class)
-                .block();
+        .block();
+*/
+        Map<String,String> map = new HashMap<>();
+        webClient.get()
+                .uri("http://localhost:8082/api/inventory/all")
+                .retrieve()
+                .bodyToMono(Map.class)
+                .subscribe(m->m.putAll(map));
 
         System.out.println("response from Inventory service");
-        result.forEach((k,v) -> System.out.println(k + " : " + v));
-
+        map.forEach((k,v)-> System.out.println(k+""+v));
         System.out.println("order placed");
     }
 }
